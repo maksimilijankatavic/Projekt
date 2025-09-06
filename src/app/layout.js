12208e51bb3192b306bfc9abe32803178cb6fc94
Maksimilijan Katavić <katavicmax@gmail.com>
2025-09-06 17:28:42 +0200

    Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1a1a1a]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030712]`}
       >
         <Background />
         <div className="relative z-10">
