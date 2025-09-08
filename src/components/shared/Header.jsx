6351275ba06c34f185e80cda4cd92731dfe2b83e
Maksimilijan Katavić <katavicmax@gmail.com>
2025-09-08 14:00:06 +0200

    Poruka

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "../ui/button"
+import { Button } from "@/components/ui/button"
 
 export default function Header() {
     return(
