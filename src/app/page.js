f1caf9873ff24a2e6c20da17f8e7ea48733de0f5
Maksimilijan Katavić <katavicmax@gmail.com>
2025-09-09 19:36:21 +0200

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -87,7 +87,30 @@ export default function Home() {
                   className="cursor-pointer text-gray-400 bg-orange-500/5 hover:text-orange-400 hover:bg-orange-500/10" 
                   aria-label="Submit text for analysis"
                 >
-                  {loading ? "..." : <CircleArrowUp />}
+                  {loading ? (
+                    <svg 
+                      className="w-5 h-5 animate-spin text-gray-400" 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      fill="none" 
+                      viewBox="0 0 24 24"
+                    >
+                      <circle 
+                        className="opacity-25" 
+                        cx="12" 
+                        cy="12" 
+                        r="10" 
+                        stroke="currentColor" 
+                        strokeWidth="4"
+                      ></circle>
+                      <path 
+                        className="opacity-75" 
+                        fill="currentColor" 
+                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                      ></path>
+                    </svg>
+                  ) : (
+                    <CircleArrowUp />
+                  )}
                 </Button>
               </div>
             </div>
